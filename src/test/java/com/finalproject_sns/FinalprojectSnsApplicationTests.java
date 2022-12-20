0919f90f94bc22fdef70fb783328bb94d57ba679
ajin0516 <dkwls7432@naver.com>
2022-12-20 14:16:07 +0900

    first commit

---
+++ src/test/java/com/finalproject_sns/FinalprojectSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject_sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
