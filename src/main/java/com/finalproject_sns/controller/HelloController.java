2160e3f4b49c8691bb9a5fd48279e087edbc4391
ajin0516 <dkwls7432@naver.com>
2022-12-20 16:23:48 +0900

    [feat] HelloController

--- src/main/java/com/finalproject_sns/controller/HelloController.java
+++ src/main/java/com/finalproject_sns/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HelloController {
 
-    @GetMapping("/users")
+    @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
