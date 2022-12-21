f5c3086c218354311b77dc3e2ab79ce16ddb7c72
ajin0516 <dkwls7432@naver.com>
2022-12-21 22:51:11 +0900

    [FIX] 어노테이션 추가

--- src/main/java/com/finalproject_sns/domain/dto/UserLoginRequest.java
+++ src/main/java/com/finalproject_sns/domain/dto/UserLoginRequest.java
@@ -2,9 +2,11 @@ package com.finalproject_sns.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class UserLoginRequest {
 
     private String userName;
