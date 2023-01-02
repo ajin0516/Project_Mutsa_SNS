ee67cdda95c044f7bb32101cf41cca78138bd97a
ajin0516 <dkwls7432@naver.com>
2023-01-02 16:48:07 +0900

    [Feat] ErrorCode 추가

--- src/main/java/com/finalproject_sns/exception/ErrorCode.java
+++ src/main/java/com/finalproject_sns/exception/ErrorCode.java
@@ -14,7 +14,9 @@ public enum ErrorCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러");
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
+    UNKNOWN_ERROR(HttpStatus.PROCESSING,"알 수 없는 에러입니다,"),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다");
 
     private HttpStatus httpStatus;
     private String message;
