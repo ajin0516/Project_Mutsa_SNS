b5a4a876554af8fb82cd15b94ee42eaedeb9285f
ajin0516 <dkwls7432@naver.com>
2023-01-06 19:53:10 +0900

    [Feat] ErrorCode 추가

--- src/main/java/com/finalproject_sns/exception/ErrorCode.java
+++ src/main/java/com/finalproject_sns/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "UserName이 중복됩니다."), // 409
+    ALREADY_LIKE_CLICK(HttpStatus.CONFLICT, "이미 좋아요를 눌렀습니다."),
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "Not founded"),  // 404
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 댓글이 없습니다."),
@@ -16,7 +17,7 @@ public enum ErrorCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"), // 500
-    UNKNOWN_ERROR(HttpStatus.PROCESSING,"알 수 없는 에러입니다,"), // 102
+    UNKNOWN_ERROR(HttpStatus.PROCESSING,"알 수 없는 에러입니다,"), // 102,
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다");
 
     private HttpStatus httpStatus;
