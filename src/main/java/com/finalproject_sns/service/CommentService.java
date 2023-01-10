6c29c5ef754e0d84cd58bcdffe365015e5d19db1
ajin0516 <dkwls7432@naver.com>
2023-01-10 14:26:35 +0900

    [Feat] ADMIN일 경우 댓글 수정, 삭제 가능하게 변경

--- src/main/java/com/finalproject_sns/service/CommentService.java
+++ src/main/java/com/finalproject_sns/service/CommentService.java
@@ -20,7 +20,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 
 @Slf4j
@@ -46,7 +45,7 @@ public class CommentService {
 
 
         Comment saveComment = commentRepository.save(commentCreateRequest.toEntity(post, user));
-        if (user.getId() != post.getUser().getId()) {  // 본인이 작성한 포스트에 본인이 댓글을 남기면 알람 저장 안함
+        if (user.getId() != post.getUser().getId()) {  // 본인이 작성한 포스트에 본인이 댓글을 남기면 댓글 알람 저장 안함
             alarmRepository.save(new Alarm(user, post, AlarmType.NEW_COMMENT_ON_POST));
         }
         return CommentCreateResponse.of(saveComment);
@@ -66,9 +65,8 @@ public class CommentService {
         // 작성자 불일치
         log.info("userName={}", userName);
         log.info("comment.getUser().getUserName()={}", comment.getUser().getUserName());
-        if (!userName.equals(comment.getUser().getUserName())) {
-            throw new AppException(ErrorCode.INVALID_PERMISSION, "사용자 권한이 없습니다.");
-        }
+
+        validateCheck(userName, comment, user);
 
         comment.update(commentUpdateRequest.getComment());
         commentRepository.save(comment);
@@ -81,7 +79,6 @@ public class CommentService {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND, "포스트가 존재하지 않습니다."));
 
-        log.info("post={}", post);
         Page<Comment> byPostId = commentRepository.findByPostId(post.getId(), pageable);
         Page<CommentListResponse> commentListResponses = CommentListResponse.toDtoList(byPostId);
         return commentListResponses;
@@ -91,25 +88,32 @@ public class CommentService {
     public CommentDeleteResponse delete(String userName, Long postId, Long commentId) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND, "존재하지 않는 포스트입니다."));
+
         User user = userRepository.findByUserName(userName)
                 .orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, userName + "는 존재하지 않는 회원입니다."));
+
         log.info("commentId={}", commentId);
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new AppException(ErrorCode.COMMENT_NOT_FOUND, "존재하지 않는 댓글입니다."));
 
-        if (!comment.getUser().getUserName().equals(userName)) {
-            throw new AppException(ErrorCode.INVALID_PERMISSION, "작성자만 삭제 가능합니다.");
-        }
-        log.info("post.getUser().getId()={}", post.getUser().getId());
-        log.info("post.getId()={}", post.getId());
+        validateCheck(userName, comment,user);
+
         Alarm alarm = alarmRepository.findByUserAndTargetId(post.getUser(), post.getId());
 
         commentRepository.delete(comment);
-        if (user.getId() != post.getUser().getId()) { // 본인이 작성한 포스트가 아닌 다른 회원의 포스트의 댓글 시 삭제
+
+        log.info("user.getId()={}, post.getUser().getId()={}",user.getId(), post.getUser().getId());
+        if (user.getId() != post.getUser().getId()) { // 본인이 작성한 포스트에 본인이 댓글 남기면 알람을 저장 안하기 때문에 삭제도 암함
             alarmRepository.delete(alarm);
         }
 
-        log.info("post.getId()={}");
         return CommentDeleteResponse.toDto(comment);
     }
+
+    private static void validateCheck(String userName, Comment comment, User user) {
+        if (!userName.equals(comment.getUser().getUserName()) && !user.getRole().equals(UserRole.ADMIN)) {
+            throw new AppException(ErrorCode.INVALID_PERMISSION, "작성자만 가능합니다.");
+        }
+    }
+
 }
